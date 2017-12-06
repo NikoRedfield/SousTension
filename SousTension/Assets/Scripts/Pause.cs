62d449c93aef19a1532907692c09ef86c5811e9f
NikoRedfield <niko-redfield@hotmail.fr>
2017-12-06 13:52:17 +0100

    Brightness and music volume controle in-game

--- SousTension/Assets/Scripts/Pause.cs
+++ SousTension/Assets/Scripts/Pause.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour {
 
     public GameObject menuToDisplay;    //UI to display for the pause menu
+    public GameObject optionPanel;
 
 
     private bool isActive = false;  //Checks if the pause is already active
     private string mainMenuScene = "Menu";
     private GameObject player;
+    private bool optionScreen = false;
     
 
     private void Start()
@@ -23,29 +26,25 @@ public class Pause : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
         if (isActive)
         {
-           
             menuToDisplay.SetActive(true);
-            //Cursor.visible = true;
-          //  Cursor.lockState = CursorLockMode.Confined;
-            //Time.timeScale = 0;
-
         }
-
         else
         {
-           
             menuToDisplay.SetActive(false);
-          //  Cursor.visible = false;
-           // Cursor.lockState = CursorLockMode.None;
-            //Time.timeScale = 1;
         }
 
-        if (CrossPlatformInputManager.GetButtonDown("Start"))   //Enable or Disable pause
+            if (CrossPlatformInputManager.GetButtonDown("Start"))   //Enable or Disable pause
         {
-            ResumeGame();
+            if (optionScreen)
+            {
+                SwitchPanel();
+            }
+            else
+            {
+                ResumeGame();
+            }
         }
 
     }
@@ -54,12 +53,34 @@ public class Pause : MonoBehaviour {
     public void ResumeGame()
     {
         isActive = !isActive;
+      
         player.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().SetAuthorisation(!isActive);
+      
+        
+        if (isActive)
+        {
+            
+            menuToDisplay.SetActive(true);
+            menuToDisplay.transform.GetChild(2).GetComponent<Button>().Select();
+            Time.timeScale = 0;
+            //Cursor.visible = true;
+            //  Cursor.lockState = CursorLockMode.Confined;
+            //Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            menuToDisplay.SetActive(false);
+            //  Cursor.visible = false;
+            // Cursor.lockState = CursorLockMode.None;
+            //Time.timeScale = 1;
+        }
     }
 
     //Load the main menu scene
     public void MainMenu()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(mainMenuScene);
     }
 
@@ -67,5 +88,25 @@ public class Pause : MonoBehaviour {
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OptionScreen()
+    {
+        optionScreen = true;
+        menuToDisplay.gameObject.SetActive(false);
+        optionPanel.SetActive(true);
+        optionPanel.transform.GetChild(2).GetComponent<Slider>().Select();
+    }
+
+    private void SwitchPanel()
+    {
+        if (optionPanel.activeSelf)
+        {
+            menuToDisplay.SetActive(true);
+            optionPanel.SetActive(false);
+            optionScreen = false;
+            menuToDisplay.transform.GetChild(2).GetComponent<Button>().Select();
+        }
+        return;
     }
 }
---
+++ SousTension/Assets/Scripts/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    private Slider brightnessSlider;
+    private Slider volumeSlider;
+
+    void Start () {
+        brightnessSlider = GameObject.Find("BrightnessSlider").GetComponent<Slider>();
+        volumeSlider = GameObject.Find("MusicVolumeSlider").GetComponent<Slider>();
+        brightnessSlider.Select();
+    }
+
+
+    public void AdjustAmbientLight()
+    {
+        RenderSettings.ambientLight = new Color(brightnessSlider.value,brightnessSlider.value,brightnessSlider.value, 1);
+    }
+
+    public void AdjustVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
