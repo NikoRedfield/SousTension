a0e8c1eccfff516fd1f22e09057dc221505e85bb
NikoRedfield <niko-redfield@hotmail.fr>
2018-01-22 14:46:29 +0100

    Modify Main Camera Process

--- SousTension/Assets/Scripts/Camera/CameraFollow2D.cs
+++ SousTension/Assets/Scripts/Camera/CameraFollow2D.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class CameraFollow2D : MonoBehaviour {
 
+    
+    public Camera cam;
     public Transform player;
     public Vector3 offset;
+    public float limitLeft;
+    public float limitRight;
 
     //Follow the player with a certain offset on each axes
     void Update()
     {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        if (player.position.x + offset.x > limitRight || player.position.x + offset.x < limitLeft)
+        {
+            return;
+        }
+        else
+        {
+
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        }
+        
     }
 }
---
+++ SousTension/Assets/Scripts/Camera/FixedCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour {
+
+    public Camera cam;
+    public Transform player;
+    public Vector3 offset;
+    public float limitLeft;
+    public float limitRight;
+
+    //Follow the player with a certain offset on each axes
+    void Update()
+    {
+        if(player.position.x + offset.x > limitRight || player.position.x + offset.x < limitLeft)
+        {
+            cam.GetComponent<CameraFollow2D>().enabled = false;
+        }
+        else
+        {
+            if(!cam.GetComponent<CameraFollow2D>().enabled)
+            {
+                cam.GetComponent<CameraFollow2D>().enabled = true;
+            }
+            transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+        }
+        
+    }
+
+}
