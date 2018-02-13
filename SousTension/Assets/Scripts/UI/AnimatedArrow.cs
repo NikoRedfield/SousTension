1f46d76b5b6658eee77579fde1476e28cdbea83d
NikoRedfield <niko-redfield@hotmail.fr>
2018-02-13 22:06:59 +0100

    Lungs animations

---
+++ SousTension/Assets/Scripts/UI/AnimatedArrow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedArrow : MonoBehaviour {
+
+    public int max;
+    public int min;
+
+    private bool up = true;
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.transform.localPosition.y >=max)
+        {
+            up = false;
+            Debug.Log("Reached max");
+        }
+        if(this.transform.localPosition.y <= min)
+        {
+            up = true;
+        }
+        if (up)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 3f, this.transform.position.z);
+        }
+        if(!up)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - 3f, this.transform.position.z);
+        }
+	}
+}
