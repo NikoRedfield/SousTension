746b4ea2801a3fb0fead397a71abc0bd2589c676
NikoRedfield <niko-redfield@hotmail.fr>
2018-02-14 15:29:10 +0100

    Key interaction

---
+++ SousTension/Assets/Scripts/UI/Key.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour {
+
+    public GameObject PickedUpKey;
+
+	// Use this for initialization
+	void Start () {
+        if (PlayerData.hasKey)
+        {
+            this.GetComponent<SpriteRenderer>().enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("Player") && !PlayerData.hasKey)
+        {
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            PlayerData.hasKey = true;
+            PickedUpKey.SetActive(true);
+        }
+    }
+}
---
+++ SousTension/Assets/Scripts/UI/YouPicked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YouPicked : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().backward = false;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().SetAuthorisation(false);
+        GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets._2D.PlatformerCharacter2D>().Move(0, false, false);
+        if (Input.GetButton("Submit") || Input.GetKeyDown("c"))
+        {
+            GameObject.FindGameObjectWithTag("Player").GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().SetAuthorisation(true);
+            this.gameObject.SetActive(false);
+        }
+    }
+}
