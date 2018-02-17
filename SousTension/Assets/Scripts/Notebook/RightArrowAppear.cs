2f2b04a7d9440ac88cbc2088a58d0408cbd3e2c7
NikoRedfield <niko-redfield@hotmail.fr>
2018-02-17 18:30:26 +0100

    Various Corrections

---
+++ SousTension/Assets/Scripts/Notebook/RightArrowAppear.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightArrowAppear : MonoBehaviour {
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (PlayerData.Ejournal)
+        {
+            this.GetComponent<Image>().enabled = true;
+        }
+        else
+        {
+            this.GetComponent<Image>().enabled = false;
+        }
+	}
+}
