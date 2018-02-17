14d4b713c735ff8784fb338ea48a15f3f3e65d68
NikoRedfield <niko-redfield@hotmail.fr>
2018-02-17 21:31:49 +0100

    Conclusion

--- SousTension/Assets/Scripts/Notebook/ConclusionEnquete.cs
+++ SousTension/Assets/Scripts/Notebook/ConclusionEnquete.cs
@@ -5,12 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class ConclusionEnquete : MonoBehaviour {
 
+    public GameObject Pict1;
+    public GameObject Pict2;
+    public GameObject unknown1;
+    public GameObject unknown2;
     public GameObject indice1;
     public GameObject indice2;
     public GameObject indice3;
     public GameObject indice4;
     public GameObject conclusion1;
     public GameObject conclusion2;
+    public GameObject conclusionUnknown;
+    public GameObject conclusionPartiel;
     public string NextScene;
 
     // Use this for initialization
@@ -30,27 +36,47 @@ public class ConclusionEnquete : MonoBehaviour {
     {
         if (PlayerData.Sang1)
         {
+            Pict1.SetActive(true);
             indice1.SetActive(true);
         }
         if (PlayerData.Sang2)
         {
+            Pict1.SetActive(true);
             indice2.SetActive(true);
+        }
+        if(!PlayerData.Sang1 && !PlayerData.Sang2)
+        {
+            unknown1.SetActive(true);
         }
         if (PlayerData.Journal1)
         {
+            Pict2.SetActive(true);
             indice3.SetActive(true);
         }
         if (PlayerData.Journal2)
         {
+            Pict2.SetActive(true);
             indice4.SetActive(true);
+        }
+        if(!PlayerData.Journal1 && !PlayerData.Journal2)
+        {
+            indice2.SetActive(true);
         }
         if(PlayerData.Sang1 && PlayerData.Journal1)
         {
             conclusion1.SetActive(true);
         }
-        if(PlayerData.Journal2 && PlayerData.Sang2)
+        if((PlayerData.Sang1 && PlayerData.Journal2)||(PlayerData.Sang2 && PlayerData.Journal1)|| (PlayerData.Journal2 && PlayerData.Sang2))
         {
             conclusion2.SetActive(true);
+        }
+        if(((PlayerData.Sang1 || PlayerData.Sang2) && !PlayerData.Journal1 && !PlayerData.Journal2)||((PlayerData.Journal1 || PlayerData.Journal2) && !PlayerData.Sang1 && !PlayerData.Sang2))
+        {
+            conclusionPartiel.SetActive(true);
+        }
+        if(!PlayerData.Sang1 && !PlayerData.Sang2 && !PlayerData.Journal1 && !PlayerData.Journal2)
+        {
+            conclusionUnknown.SetActive(true);
         }
     }
 }
