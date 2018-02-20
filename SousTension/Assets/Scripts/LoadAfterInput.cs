66eef43f3fbd5872db15dccbbd387a164414d365
NikoRedfield <niko-redfield@hotmail.fr>
2018-02-20 19:27:19 +0100

    Character implementation

---
+++ SousTension/Assets/Scripts/LoadAfterInput.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadAfterInput : MonoBehaviour
+{
+
+    public string _nextScene = "";
+
+    public string input = "Submit";
+
+
+    //Load the given scene after the stated input has been made
+    private void Update()
+    {
+        if (Input.GetButton(input))
+        {
+            SceneManager.LoadScene(_nextScene);
+        }
+        
+    }
+}
